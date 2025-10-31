892e752177dce8da406a4b7e0e33c59b59a96f3d
Bela Bohlender <bela@bohlender.email>
2025-11-01 00:12:01 +0100

    fix: switch and queue components

--- packages/react/src/queue.tsx
+++ packages/react/src/queue.tsx
@@ -1,8 +1,10 @@
 import { QueueTimeline } from '@pmndrs/timeline'
 import { forwardRef, useImperativeHandle, useMemo } from 'react'
+import { useAttachTimeline } from './attachable.js'
 
 export const Queue = forwardRef<QueueTimeline, {}>((_, ref) => {
   const queue = useMemo(() => new QueueTimeline(), [])
   useImperativeHandle(ref, () => queue, [queue])
+  useAttachTimeline(() => queue.run(), [])
   return null
 })
--- packages/react/src/switch.tsx
+++ packages/react/src/switch.tsx
@@ -1,13 +1,14 @@
 import { SwitchTimelineCaseCondition, SwitchTimeline } from '@pmndrs/timeline'
 import { RootState } from '@react-three/fiber'
 import { createContext, forwardRef, ReactNode, useContext, useImperativeHandle, useMemo, useRef } from 'react'
-import { Attachable, AttachableProvider } from './attachable.js'
+import { Attachable, AttachableProvider, useAttachTimeline } from './attachable.js'
 
 export const SwitchContext = createContext<SwitchTimeline<RootState> | undefined>(undefined)
 
 export const Switch = forwardRef<SwitchTimeline<RootState>, { children?: ReactNode }>(({ children }, ref) => {
   const _switch = useMemo(() => new SwitchTimeline<RootState>(), [])
   useImperativeHandle(ref, () => _switch, [_switch])
+  useAttachTimeline(() => _switch.run(), [_switch])
   return <SwitchContext.Provider value={_switch}>{children}</SwitchContext.Provider>
 })
 
@@ -20,19 +21,18 @@ export function SwitchCase({
   condition?: SwitchTimelineCaseCondition<RootState>
   children?: ReactNode
 }) {
-  const sequential = useContext(SwitchContext)
-  if (sequential == null) {
+  const _switch = useContext(SwitchContext)
+  if (_switch == null) {
     throw new Error(`SwitchCase can only be used inside the Switch component.`)
   }
   const conditionRef = useRef(condition)
   conditionRef.current = condition
   const attachable = useMemo<Attachable>(
     () => ({
-      attach: (timeline) =>
-        sequential.attach(index, (...params) => conditionRef.current?.(...params) ?? true, timeline),
-      unattach: () => sequential.unattach(index),
+      attach: (timeline) => _switch.attach(index, (...params) => conditionRef.current?.(...params) ?? true, timeline),
+      unattach: () => _switch.unattach(index),
     }),
-    [sequential, index],
+    [_switch, index],
   )
   return <AttachableProvider attachable={attachable}>{children}</AttachableProvider>
 }
